605f78ffe5a13f39283ca2cfa0f1ffdc02f82e44
SunnyJinny <hyejin.kang@rwth-aachen.de>
2023-04-15 19:52:49 +0200

    feat: add new components

---
+++ clickdone/src/app/studenten/studenten.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StudentenComponent } from './studenten.component';
+
+describe('StudentenComponent', () => {
+  let component: StudentenComponent;
+  let fixture: ComponentFixture<StudentenComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StudentenComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(StudentenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ clickdone/src/app/studenten/studenten.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studenten',
+  templateUrl: './studenten.component.html',
+  styleUrls: ['./studenten.component.scss']
+})
+export class StudentenComponent {
+
+}
