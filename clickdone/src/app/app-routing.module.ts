645011c24674de4176bae7265684bec88d6c878a
SunnyJinny <hyejin.kang@rwth-aachen.de>
2023-04-15 19:53:42 +0200

    feat: add routing from nav to body content page

--- clickdone/src/app/app-routing.module.ts
+++ clickdone/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { StudentenComponent } from './studenten/studenten.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'documents', component: DocumentsComponent},
+  {path: 'students', component: StudentenComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
