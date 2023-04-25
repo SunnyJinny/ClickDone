41ca60dabbcea92836edf966bf1be9a21d801def
SunnyJinny <hyejin.kang@rwth-aachen.de>
2023-04-25 23:45:47 +0200

    feat: create list component

---
+++ clickdone/src/app/list-add-edit/list-add-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-add-edit',
+  templateUrl: './list-add-edit.component.html',
+  styleUrls: ['./list-add-edit.component.scss']
+})
+export class ListAddEditComponent {
+
+}
